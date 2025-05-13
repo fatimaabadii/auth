edc8534fded09f8ed84a77685559170e7a40493b
fatimaabadii <fatimaabadi.tima98@gmail.com>
2025-05-13 20:19:35 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -14,11 +15,19 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 
-// Protected example route
 const verifyToken = require('./middlewares/authMiddleware');
+
 app.get('/api/protected', verifyToken, (req, res) => {
-  res.json({ message: `Welcome ${req.user.username}! This is protected.` });
+  console.log('User data from token:', req.user); 
+
+  if (req.user) {
+    return res.json({ message: `Welcome ${req.user.username}! This is protected.` });
+  } else {
+    return res.status(400).json({ message: 'User data not found.' });
+  }
 });
+
+
 
 // Connect MongoDB and Start server
 mongoose.connect(process.env.MONGO_URI)
